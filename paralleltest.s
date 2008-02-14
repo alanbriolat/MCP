66ccabcc038049c6fd45cb23e7e60102c7e89a3d
Alan Briolat <alan.briolat@gmail.com>
2008-02-14 11:35:56 +0000

    Test changes

--- paralleltest.s
+++ paralleltest.s
@@ -1,15 +1,25 @@
-.globl parallel_init, parallel_outa
+.globl parallel_init, parallel_outa, parallel_outb
 .globl delay
 
 start:
     ld sp, 0xffff
     call parallel_init
+    ld a, 0x90
+    call parallel_outa
 0:  ld a, 0x00
-    call parallel_outa
+    call parallel_outb
+    ld b, 0x06
+    call delay
     ld a, 0x7f
-    call parallel_outa
+    call parallel_outb
+    ld b, 0x06
+    call delay
     ld a, 0xff
-    call parallel_outa
+    call parallel_outb
+    ld b, 0x06
+    call delay
     ld a, 0x7f
-    call parallel_outa
+    call parallel_outb
+    ld b, 0x06
+    call delay
     jr 0b
